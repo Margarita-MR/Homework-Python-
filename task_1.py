3674a34fe7f8175740eb59f597f7c0bc7cc48a72
MargaritaM <ya.margarita1005@gmail.com>
2022-12-12 22:29:17 +0300

    Доработана 1 задача, так как обнаруженная ошибка

--- task_1.py
+++ task_1.py
@@ -1,13 +1,37 @@
 # Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 
-wd=[1,2,3,4,5]
+# Вариант 1
+
 print('Введите номер дня недели - ',end='') 
-d=int(input())
-print(d,end='')
-if d:=wd:
-    print(' -> нет')
+day=int(input())
+# norm_wd = [1,5]
+# norm_sd = [6,7]
+if day == 1 or day == 2 or day == 3 or day == 4 or day == 5: 
+    print(f'{day} -> нет')
+
+elif day == 6 or day == 7: 
+    print(f'{day} -> да')
 else:
-    print(' -> да')
- 
+    print('В неделе 7 дней!!!')
 
 
+#  Вариант 2
+
+print('Введите номер дня недели - ',end='') 
+day=int(input())
+if day==1:
+    print('нет')
+elif day==2:
+    print('нет')
+elif day==3:
+    print('нет')
+elif day==4:
+    print('нет')
+elif day==5:
+    print('нет')
+elif day==6:
+    print('да')
+elif day==7:
+    print('да')
+else: 
+    print('в неделе 7 дней')
